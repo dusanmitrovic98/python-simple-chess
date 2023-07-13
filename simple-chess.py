55bad2c84ee3e8903904700181eac6ad490e4919
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-13 23:46:06 +0200

    Update simple-chess.py

--- simple-chess.py
+++ simple-chess.py
@@ -2,3 +2,4 @@ class ChessGame:
     def __init__(self):
         self.board = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
                       ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
