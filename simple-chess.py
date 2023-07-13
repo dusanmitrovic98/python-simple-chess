5837fac5f1db989a20bbbccfe98d7d2a30a9d267
Dušan Mitrović (Dusan Mitrovic) <62337583+dusanmitrovic98@users.noreply.github.com>
2023-07-13 23:57:13 +0200

    Update simple-chess.py

--- simple-chess.py
+++ simple-chess.py
@@ -23,3 +23,29 @@ class ChessGame:
         print("   ".join([chr(97 + i) for i in range(8)]))
 
     def move_piece(self, from_pos, to_pos):
+        from_col, from_row = self.parse_position(from_pos)
+        to_col, to_row = self.parse_position(to_pos)
+
+        piece = self.board[from_row][from_col]
+        self.board[from_row][from_col] = ' '
+        self.board[to_row][to_col] = piece
+
+    def parse_position(self, position):
+        col = ord(position[0]) - ord('a')
+        row = 8 - int(position[1])
+        return col, row
+
+    def play(self):
+        self.print_board()
+
+        while True:
+            from_pos = input("Enter the position of the piece you want to move: ")
+            to_pos = input("Enter the target position: ")
+
+            self.move_piece(from_pos, to_pos)
+            self.print_board()
+
+
+# Start the game
+chess_game = ChessGame()
+chess_game.play()
